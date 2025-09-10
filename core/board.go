1310f8f62381deee3036198b7dd822fe2a24a86c
Nachmias Lior <lias1225@elta.co.il>
2025-09-10 15:16:13 +0300

    first commit

---
+++ core/board.go
@@ -0,0 +1,107 @@
+package core
+
+// C:\Users\Lias1225\sources\go\bin 
+
+type PieceType int
+
+const (
+	NoPiece PieceType = iota
+	King
+	Queen
+	Rook
+	Knight
+	Bishop
+	Pawn
+)
+
+type Color int
+const (
+	NoColor Color = iota
+	Black
+	White
+)
+
+type Piece struct {
+	pieceType PieceType
+	color Color
+}
+
+type File int
+const (
+	a File = iota
+	b
+	c
+	d
+	e
+	f
+	g
+	h
+	MaxFile
+)
+
+const MinRank = 1
+const MaxRank = 8
+
+type Square int
+
+func newSquare(rank int, file File) Square {
+	return (Square)(rank * 8 + (int)(file))
+}
+
+func (s Square) File() File {
+	return (File)(s % 8)
+}
+
+func (s Square) Rank() int {
+	return (int)(s / 8)
+}
+
+type Board struct {
+	board map[Square]Piece
+}
+
+type squareOccupiedError struct {
+}
+
+func (e squareOccupiedError) Error() string {
+	return "sqaure is already occupied"
+}
+
+type noPieceAtSquareError struct {
+}
+
+func (e noPieceAtSquareError) Error() string {
+	return "no piece in this square"
+}
+
+func (b *Board) Move(from, to Square) error {
+	if _, ok := b.board[to]; ok {
+		return squareOccupiedError{}
+	}
+	
+	if p, err := b.Take(from); err != nil {
+		return err
+	} else {
+		b.board[to] = p
+	}
+
+	return nil
+}
+
+func (b *Board) Take(from Square) (Piece, error) {
+	if p, ok := b.board[from]; !ok {
+		return Piece{}, noPieceAtSquareError{}
+	} else {
+		delete(b.board, from)
+		return p, nil
+	}
+}
+
+func (b *Board) Set(p Piece, s Square) error {
+	if _, ok := b.board[s]; ok {
+		return squareOccupiedError{}
+	}
+
+	b.board[s] = p
+	return nil
+}
